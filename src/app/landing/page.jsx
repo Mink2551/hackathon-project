d3f02a8647a4310c3d8c657d482a7c9a6e098a8d
pacharapol pimpa <thefirestong@gmail.com>
2025-02-23 17:15:32 +0700

    fixing navbar component

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -1,21 +1,9 @@
 import Navbar from '../components/navbar'
-import Link from 'next/link';
+
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-[#FAF3E0]">
-      <nav className="bg-blue-900 text-white p-4 flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-orange-500">R<span className="text-white">ush</span> Radar</h1>
-        <div className="space-x-6">
-          <Link href="/" className="hover:underline">Home</Link>
-          <Link href="/map" className="hover:underline">Map</Link>
-          <Link href="/transport" className="hover:underline">Public Transport</Link>
-          <Link href="/crowded" className="hover:underline">Crowded Zone</Link>
-          <Link href="/register" className="hover:underline">Register</Link>
-        </div>
-      </nav>
-
-
-
+        <Navbar/>
     </div>
   );
 }
